d6fedacdf56055292c2260dc3cc5ab319e78fcd8
Arafat islam <arafat.innotimes.net@gmail.com>
2024-07-01 08:27:03 +0600

    user created

--- index.js
+++ index.js
@@ -11,3 +11,64 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ykgi9mv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+
+    const usersCollection = client.db('assainment12').collection('user')
+
+
+    // jwt
+    app.post('/jwt', async (req, res) => {
+      const userInfo = req.body
+      const token = jwt.sign(userInfo, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ token })
+    })
+
+    app.get('/hrmanager/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = {
+        email: email
+      }
+      const hr = await usersCollection.findOne(query)
+      let hrRole = false
+      let employeeRole = false
+      if (hr) {
+        hrRole = hr?.role === 'hr'
+        employeeRole = hr?.role === 'employee'
+      }
+      res.send({ hrRole, employeeRole })
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('boss is sitings');
+})
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+})
